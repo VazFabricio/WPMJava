99ff1fb1fc1bc062f857894c4a6d0ccf63f41bde
fabricio vaz <fabricio.vaz2004@gmail.com>
2022-12-30 14:28:19 -0300

    Remoção da Classe GUI, para facilitar na implementação da interface gráfiaca.

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,14 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.time.LocalTime;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-public class Game {
-    final private static String[] words = {"oi", "sim", "nao", "amanha", "teste", "ontem"};
-    final private static int numberOfElements = words.length-1;
+public class Game{
+    final private static String[] WORDS = {"oi", "sim", "nao", "amanha", "teste", "ontem"};
+    final private static int NUMBER_OF_ELEMENTS = WORDS.length-1;
 
     Random rand = new Random();
 
@@ -24,10 +27,13 @@ public class Game {
     public void play(){
         //inicio jogo
 
+        window();
+
+        /*
         this.start = LocalTime.now().toNanoOfDay();
 
         for (int i = 0; i < 10; i++){
-            this.generatedWord  = words[rand.nextInt(numberOfElements)];
+            this.generatedWord  = WORDS[rand.nextInt(NUMBER_OF_ELEMENTS)];
             System.out.print( generatedWord + " ");
             System.out.println();
             String wordTyped = sc.nextLine();
@@ -45,7 +51,67 @@ public class Game {
 
         //fim jogo
 
+         */
+
     }
+
+
+    public void countdown(int countdownSec) throws InterruptedException {
+
+        for (int i = countdownSec; i > 0; i--){
+            System.out.println(i);
+            TimeUnit.SECONDS.sleep(1);
+        }
+
+    }
+    public void window(){
+        //Graph
+        //window
+        JFrame frame = new JFrame();
+        //layout
+        JPanel panel = new JPanel();
+        //frame
+        frame.setSize(1000, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(panel);
+
+        //panel
+        panel.setLayout(null);
+
+
+        //GeneratedWords
+        JLabel label = new JLabel();
+        label.setText(WORDS[rand.nextInt(NUMBER_OF_ELEMENTS)]);
+        label.setBounds(200, 20, 80, 25);
+        panel.add(label);
+
+
+        //Input
+        JTextField type = new JTextField();
+        type.setBounds(200, 60, 165, 25);
+        type.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyChar() == KeyEvent.VK_ENTER){
+                    label.setText(WORDS[rand.nextInt(NUMBER_OF_ELEMENTS)]);
+                }
+            }
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+        panel.add(type);
+
+        //end Graph
+    }
+
     public int wpm(){
         double totalTime = end - start;
         double seconds = totalTime / 1000000000.0;
@@ -57,19 +123,5 @@ public class Game {
         return wpm;
     }
 
-    public void countdown(int countdownSec) throws InterruptedException {
-
-        for (int i = countdownSec; i > 0; i--){
-            System.out.println(i);
-            TimeUnit.SECONDS.sleep(1);
-        }
-
-    }
-
-    //Getters and Setters
-
-
-    public String getGeneratedWord() {
-        return generatedWord;
-    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Game game = new Game();
-
-        //GUI.window(game);
-
-
-
-        game.countdown(4);
         game.play();
-        System.out.println(game.wpm());
-
 
 
     }
